9f177008c162ac9e13b7e568bc9f7ba001c9db62
Yatharth2112 <yatharthshaandily2007@gmail.com>
2026-08-14 08:01:31 +0530

    Drink water app created

--- Hand on Projects/Drink Water.py
+++ Hand on Projects/Drink Water.py
@@ -3,6 +3,6 @@ from plyer import notification
 
 while True:
     print("Please sip some water!")
-    notification.notify(Title = "Please drink some water", message = "You need to drink some water")
-    time.sleep(60 * 60)
+    notification.notify(title = "Please drink some water", message = "You need to drink some water")
+    time.sleep(3)
     
